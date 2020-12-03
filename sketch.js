dfedba81b713232ed8986f0da87e2759e7ed16d2
vharsha2008 <75422630+vharsha2008@users.noreply.github.com>
2020-12-03 02:52:31 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,195 +1,149 @@
+//please note the following things:
+
+// 1. The bananas are spawning in random positions within the 80 frames.
+// 2. When you collide with the stones in this game the game is over.
+// Have fun!
+var monkey,monkey_running,moving;
+var ground,groundImage;
+var banana,bananaImage,bananaGroup;
+var obstacle,obstacleImage,obstacleGroup, invisibleGround;
+var score = 0;
+var survivalTime = 0;
 var PLAY = 1;
 var END = 0;
-var gameState = PLAY;
-
-var trex, trex_running, trex_collided;
-var ground, invisibleGround, groundImage;
-
-var cloudsGroup, cloudImage;
-var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
-
-var score=0;
-
-var gameOver, restart;
-
-localStorage["HighestScore"] = 0;
+var gamestate = PLAY;
+var gameover = "nice try";
 
 function preload(){
-  trex_running =   loadAnimation("trex1.png","trex3.png","trex4.png");
-  trex_collided = loadAnimation("trex_collided.png");
+monkey_running = loadAnimation("monkey_0.png","monkey_1.png","monkey_2.png","monkey_3.png","monkey_4.png","monkey_5.png","monkey_6.png","monkey_7.png","monkey_8.png");
+bananaImage = loadImage("banana.png");  
+obstacleImage = loadImage("obstacle.png");  
+ groundImage = loadImage("ground2.png");
   
-  groundImage = loadImage("ground2.png");
-  
-  cloudImage = loadImage("cloud.png");
-  
-  obstacle1 = loadImage("obstacle1.png");
-  obstacle2 = loadImage("obstacle2.png");
-  obstacle3 = loadImage("obstacle3.png");
-  obstacle4 = loadImage("obstacle4.png");
-  obstacle5 = loadImage("obstacle5.png");
-  obstacle6 = loadImage("obstacle6.png");
-  
-  gameOverImg = loadImage("gameOver.png");
-  restartImg = loadImage("restart.png");
+ 
 }
+
 
 function setup() {
-  createCanvas(600, 200);
+  createCanvas(500,500);
+//creating ground
+  ground=createSprite(400,350,900,10);
+  ground.velocityX=-5;
+  ground.x=ground.width/2;
+   ground.addImage("ground",groundImage);
+  console.log(ground.x);
   
-  trex = createSprite(50,180,20,50);
+ //creating monkey
+  monkey = createSprite(80,315,20,20);
+  monkey.addAnimation("moving",monkey_running);
+  monkey.scale=0.1;
   
-  trex.addAnimation("running", trex_running);
-  trex.addAnimation("collided", trex_collided);
-  trex.scale = 0.5;
-  
-  ground = createSprite(200,180,400,20);
-  ground.addImage("ground",groundImage);
-  ground.x = ground.width /2;
-  ground.velocityX = -(6 + 3*score/100);
-  
-  gameOver = createSprite(300,100);
-  gameOver.addImage(gameOverImg);
-  
-  restart = createSprite(300,140);
-  restart.addImage(restartImg);
-  
-  gameOver.scale = 0.5;
-  restart.scale = 0.5;
-
-  gameOver.visible = false;
-  restart.visible = false;
-  
-  invisibleGround = createSprite(200,190,400,10);
+  //creating banana & obstacle group
+  bananaGroup = createGroup();
+  obstacleGroup = createGroup();
+  invisibleGround = createSprite(400,360,900,10);
   invisibleGround.visible = false;
+
+ 
   
-  cloudsGroup = new Group();
-  obstaclesGroup = new Group();
   
-  score = 0;
 }
+
 
 function draw() {
-  //trex.debug = true;
-  background(255);
-  text("Score: "+ score, 500,50);
+  background("white");
+
+  //when we press space the monkey will jump upwards.
+  if(keyDown("space")&& monkey.y >= 200)
+     {
+     monkey.velocityY=-10;
+     }
+   
   
-  if (gameState===PLAY){
-    score = score + Math.round(getFrameRate()/60);
-    ground.velocityX = -(6 + 3*score/100);
-  
-    if(keyDown("space") && trex.y >= 159) {
-      trex.velocityY = -12;
-    }
-  
-    trex.velocityY = trex.velocityY + 0.8
-  
-    if (ground.x < 0){
-      ground.x = ground.width/2;
-    }
-  
-    trex.collide(invisibleGround);
-    spawnClouds();
-    spawnObstacles();
-  
-    if(obstaclesGroup.isTouching(trex)){
-        gameState = END;
-    }
+  //to help pull the monkey down by gravity so it doesn't stay up.
+  monkey.velocityY = monkey.velocityY+0.8;
+  //resetting the ground to half it's width.
+  if (ground.x < 0)
+  {
+    ground.x = ground.width/2;
   }
-  else if (gameState === END) {
-    gameOver.visible = true;
-    restart.visible = true;
-    
-    //set velcity of each game object to 0
-    ground.velocityX = 0;
-    trex.velocityY = 0;
-    obstaclesGroup.setVelocityXEach(0);
-    cloudsGroup.setVelocityXEach(0);
-    
-    //change the trex animation
-    trex.changeAnimation("collided",trex_collided);
-    
-    //set lifetime of the game objects so that they are never destroyed
-    obstaclesGroup.setLifetimeEach(-1);
-    cloudsGroup.setLifetimeEach(-1);
-    
-    if(mousePressedOver(restart)) {
-      reset();
-    }
+// we are calling our functions up here.
+  food();
+  spawnRocks();
+
+  if(gamestate===PLAY){
+    gameover.visible=false;
+
+  if(bananaGroup.isTouching(monkey)){
+      
+    bananaGroup.destroyEach();
+    survivalTime = survivalTime+2;
   }
+}
+    if (obstacleGroup.isTouching(monkey)) {
+     gamestate=END
+     obstacleGroup.destroyEach();
   
+  } if(gamestate===END){
+    
+    monkey.destroy();
+    ground.destroy();
+    bananaGroup.destroyEach();
+    obstacleGroup.destroyEach();
+    gameover.visible=true;
+    stroke("black");
+    textSize(50);
+    fill("black");
+    text("Gameover: " + gameover,20,200);
+
+    
+  }
+  //this is to help display the survival time text on the canvas. 
+  stroke("white");
+  textSize(20);
+  fill("white");
+  text("Score: "+ score,500,50);
   
+  stroke("black");
+  textSize(20);
+  fill("black");
+  text("Survival Time: " + survivalTime,200,50);
+  
+ 
+  
+         
+  
+// this is used so the monkey can collide with the ground
+  monkey.collide( invisibleGround);
   drawSprites();
+   
 }
 
-function spawnClouds() {
-  //write code here to spawn the clouds
-  if (frameCount % 60 === 0) {
-    var cloud = createSprite(600,120,40,10);
-    cloud.y = Math.round(random(80,120));
-    cloud.addImage(cloudImage);
-    cloud.scale = 0.5;
-    cloud.velocityX = -3;
-    
-     //assign lifetime to the variable
-    cloud.lifetime = 200;
-    
-    //adjust the depth
-    cloud.depth = trex.depth;
-    trex.depth = trex.depth + 1;
-    
-    //add each cloud to the group
-    cloudsGroup.add(cloud);
-  }
-  
-}
+     
+function food(){
+  //this is to make sure the banana appears for every 80 frames.
+   if(World.frameCount%80==0){
+ 
+  banana = createSprite(300,130,20,20);
+  banana.addImage(bananaImage); 
+  banana.scale=0.1;
+  banana.y = Math.round(random(120,200));
+  banana.velocityX=-5;
+  banana.lifetime=150;
 
-function spawnObstacles() {
-  if(frameCount % 60 === 0) {
-    var obstacle = createSprite(600,165,10,40);
-    //obstacle.debug = true;
-    obstacle.velocityX = -(6 + 3*score/100);
-    
-    //generate random obstacles
-    var rand = Math.round(random(1,6));
-    switch(rand) {
-      case 1: obstacle.addImage(obstacle1);
-              break;
-      case 2: obstacle.addImage(obstacle2);
-              break;
-      case 3: obstacle.addImage(obstacle3);
-              break;
-      case 4: obstacle.addImage(obstacle4);
-              break;
-      case 5: obstacle.addImage(obstacle5);
-              break;
-      case 6: obstacle.addImage(obstacle6);
-              break;
-      default: break;
-    }
-    
-    //assign scale and lifetime to the obstacle           
-    obstacle.scale = 0.5;
-    obstacle.lifetime = 300;
-    //add each obstacle to the group
-    obstaclesGroup.add(obstacle);
+  bananaGroup.add(banana);  
+      
   }
 }
+function spawnRocks(){
+  // this is to make sure the obstacle appears after every 300 frames.
+  if(World.frameCount%60==0){
+    obstacle = createSprite(400,330,10,40);
+    obstacle.addImage(obstacleImage);
+    obstacle.scale=0.1;
+    obstacle.velocityX=-5;
+    obstacle.lifetime=150;
+    obstacleGroup.add(obstacle);
 
-function reset(){
-  gameState = PLAY;
-  gameOver.visible = false;
-  restart.visible = false;
-  
-  obstaclesGroup.destroyEach();
-  cloudsGroup.destroyEach();
-  
-  trex.changeAnimation("running",trex_running);
-  
-  if(localStorage["HighestScore"]<score){
-    localStorage["HighestScore"] = score;
   }
-  console.log(localStorage["HighestScore"]);
-  
-  score = 0;
-  
 }
